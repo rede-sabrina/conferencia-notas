53cf603ae0db54399374583b53b624741de8c337
Rede Sabrina <bruno.redesabrina@gmail.com>
2026-06-17 14:49:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ arquivo = st.file_uploader(
 
 if arquivo:
     df = pd.read_excel(arquivo)
-
+    colunas = ["Chave Acesso", "Status", "Número"]
+    df = df[colunas]
     st.write("Colunas encontradas:")
+    
     st.write(df.columns)
+    
+    st.write(f"Quantidade de notas encontradas: {len(df)}")
 
     chave = st.text_input(
         "Bipe a nota",
@@ -22,11 +26,10 @@ if arquivo:
     )
 
     if chave:
-        # Substitua pelos nomes reais das colunas da sua planilha
-        resultado = df[df["CHAVE DE ACESSO"].astype(str) == chave]
+        resultado = df[df["Chave Acesso"].astype(str) == chave]
 
         if not resultado.empty:
-            status = resultado.iloc[0]["STATUS"]
+            status = resultado.iloc[0]["Status"]
             st.success(f"Nota encontrada! Status: {status}")
         else:
             st.error("Nota não encontrada.")
